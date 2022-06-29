0bef588591742e78dc14023b8ea95bacc66bfa54
hou <41228208+cowFarmer@users.noreply.github.com>
2022-06-29 12:12:56 +0900

    2022.06.29 rm dir

--- coding_test/README.md
+++
@@ -1,2 +0,0 @@
-# 코딩 테스트로 시작하는 파이썬 프로그래밍
-![책 소개](https://book.naver.com/bookdb/book_detail.nhn?bid=22364773)
--- coding_test/ch08/code8.3_email addresses_search fucntion (ecoo19r2p1).py
+++
@@ -1,7 +0,0 @@
-def search(collection, value):
-    """
-    collection에서 value를 여러 번 검색합니다.
-    """
-    for i in range(50000):
-        found = value in collection
-        
